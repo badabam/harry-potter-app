22dc630583832fbdc54d72466023bf13c943d2c7
Jeremias Erbs <jerry@badabam.de>
2021-02-10 12:41:41 +0100

    Cleanup fetch and add Grid and createElement

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import AppHeader from './components/AppHeader'
-
-const charactersUrl = 'http://hp-api.herokuapp.com/api/characters'
+import getCharacters from './services/getCharacters'
 
 export default function App() {
   const header = AppHeader('Harry Potter App')
   document.body.append(header)
 
   // fetch API
-  const promise = fetch(charactersUrl)
-  promise.then(onResponse)
-  promise.catch(onError)
-
-  // response object
-  function onResponse(res) {
-    const promise = res.json()
-    promise.then(onData)
-    promise.catch(onError)
-  }
-
-  function onData(data) {
-    console.log(data)
-  }
-
-  function onError(error) {
-    console.log(error)
-  }
+  getCharacters()
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
 }
