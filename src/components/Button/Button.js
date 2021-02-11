0608d1bca8b7cb52a9ce3b94886479f4e781daf0
Jeremias Erbs <jerry@badabam.de>
2021-02-11 12:49:43 +0100

    Create HouseFilter component and update Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,18 @@
 import './Button.css'
 import createElement from '../../lib/createElement'
 
-export default function Button(text) {
-  return createElement('button', { className: 'Button' }, text)
+export default function Button(text, onClick) {
+  const el = createElement('button', { className: 'Button' }, text)
+
+  el.addEventListener('click', onClick)
+
+  function toggle() {
+    el.classList.toggle('selected')
+  }
+
+  function updateText(text) {
+    el.innerText = text
+  }
+
+  return { el, updateText, toggle }
 }
